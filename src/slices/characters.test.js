d1ff7fa7d74b770edf2bb776a08e6310eeaa2cc3
Carlos <carcarbe@gmail.com>
2021-07-24 00:19:12 +0200

    clean code

--- src/slices/characters.test.js
+++ src/slices/characters.test.js
@@ -1,32 +1,3 @@
-
-// import { createReducer } from '@reduxjs/toolkit'
-
-// const initialState = [
-//   {
-//     text: 'Use Redux',
-//     completed: false,
-//     id: 0
-//   }
-// ]
-
-// const todosSlice = createSlice({
-//   name: 'todos',
-//   initialState,
-//   reducers: {
-//     todoAdded(state, action: PayloadAction<string>) {
-//       state.push({
-//         id: state.reduce((maxId, todo) => Math.max(todo.id, maxId), -1) + 1,
-//         completed: false,
-//         text: action.payload
-//       })
-//     }
-//   }
-// })
-
-// export const { todoAdded } = todosSlice.actions
-
-// export default todosSlice.reducer
-/********************* */
 import { getCharacters, getCharactersSuccess, getCharactersFailure, saveCharacterAction } from './characters';
 import { store } from '../store';
 
